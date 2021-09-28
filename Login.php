3321dddd931da05b158c0fb166e8a72348bfe108
PaulaRocha19 <91512993+PaulaRocha19@users.noreply.github.com>
2021-09-28 16:37:21 -0500

    Nombres

--- Login.php
+++ Login.php
@@ -34,13 +34,13 @@
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
                         <form id="login-form" class="form" action="" method="post">
-                            <h3 class="text-center text-info">Login</h3>
+                            <h3 class="text-center text-info">Inicio de sesión</h3>
                             <div class="form-group">
-                                <label for="username" class="text-info">Username:</label><br>
+                                <label for="username" class="text-info">Usuario:</label><br>
                                 <input name="txtUsuario" id="txtUsuario" type="text" pattern="[A-Za-z9-0]" class="form-control">
                             </div>
                             <div class="form-group">
-                                <label for="password" class="text-info">Password:</label><br>
+                                <label for="password" class="text-info">Contraseña:</label><br>
                                 <input name="txtClave" id="txtClave" type="password" pattern="[A-Za-z9-0]"class="form-control">
                                 <br>
                             </div>
