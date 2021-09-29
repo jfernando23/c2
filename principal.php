d02d87dbc0b941ff46abc12f36eab264c14ab70e
PaulaRocha19 <91512993+PaulaRocha19@users.noreply.github.com>
2021-09-29 13:03:27 -0500

    Mi perdil

--- principal.php
+++ principal.php
@@ -394,63 +394,49 @@
       <div class="container">
 
         <div class="section-title">
-          <h2>Contact</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <h2>Mi Pérfil</h2>
         </div>
 
         <div class="row" data-aos="fade-in">
-
-          <div class="col-lg-5 d-flex align-items-stretch">
-            <div class="info">
-              <div class="address">
-                <i class="bi bi-geo-alt"></i>
-                <h4>Location:</h4>
-                <p>A108 Adam Street, New York, NY 535022</p>
-              </div>
-
-              <div class="email">
-                <i class="bi bi-envelope"></i>
-                <h4>Email:</h4>
-                <p>info@example.com</p>
-              </div>
-
-              <div class="phone">
-                <i class="bi bi-phone"></i>
-                <h4>Call:</h4>
-                <p>+1 5589 55488 55s</p>
-              </div>
-
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
-            </div>
-
-          </div>
-
           <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+
             <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="form-group col-md-6">
-                  <label for="name">Your Name</label>
-                  <input type="text" name="name" class="form-control" id="name" required>
-                </div>
-                <div class="form-group col-md-6">
-                  <label for="name">Your Email</label>
-                  <input type="email" class="form-control" name="email" id="email" required>
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="name">Subject</label>
-                <input type="text" class="form-control" name="subject" id="subject" required>
-              </div>
-              <div class="form-group">
-                <label for="name">Message</label>
-                <textarea class="form-control" name="message" rows="10" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
+              
+            <h3 class="text-center text-info">Registro</h3>
+                            <div class="form-group">
+                                <label for="username" class="text-info">Nombres:</label><br>
+                                <input name="txtNombre" id="txtNombre" type="text" pattern="[A-Za-z9-0]" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Apellidos:</label><br>
+                                <input name="txtApellidos" id="txtApellidos" type="text" pattern="[A-Za-z9-0]"class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Correo:</label><br>
+                                <input name="txtCorreo" id="txtCorreo" type="text" pattern="[A-Za-z9-0]"class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Dirección:</label><br>
+                                <input name="txtDir" id="txtDir" type="text" pattern="[A-Za-z9-0]"class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Numero de Hijos:</label><br>
+                                <input name="txtNumHij" id="txtNumHij" type="text" pattern="[A-Za-z9-0]"class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Estado Civil:</label><br>
+                                <input name="txtEstCivil" id="txtEstCivil" type="text" pattern="[A-Za-z9-0]"class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Foto de Pérfil:</label><br>
+                                <input type="file" name="archivo[]" id="archivo[]" multiple=""class="btn btn-info btn-md">
+                                <br>
+                                <br>
+                            </div>
+                            <div class="form-group" >
+                                <input type="submit" name="btnActualizar" value="Actualizar" class="btn btn-info btn-md">
+                                <input type="submit" name="btnCambio" value="Cambiar Clave" class="btn btn-info btn-md">
+                            </div>
             </form>
           </div>
 
